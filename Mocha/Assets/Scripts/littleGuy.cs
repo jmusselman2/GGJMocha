a7146023fcfbccab6cf5fdc32da99b7a740ef7e9
kristenYu <kristen.yu@du.edu>
2018-01-27 14:28:37 -0700

    Movement done

--- Mocha/Assets/Scripts/littleGuy.cs
+++ Mocha/Assets/Scripts/littleGuy.cs
@@ -7,18 +7,27 @@ public class littleGuy: MonoBehaviour {
 	public int moveNum; 
 	public float speed; 
 
+
+	//Movement Variables
 	private Vector2 newPos;
 	private Vector2 currPos;
 	private Vector2 myPos;
+	private Vector2 midPos; 
 	private float fraction;
 	private float journeyLength;
 	private float startTime; 
-	private bool canMove; 
+	public bool canMove; 
+
+	//control variables
+	public bool hasControl;
+
+	//Collision Variables 
+
 
 
 	// Use this for initialization
 	void Start () {
-		moveNum = 10;
+		moveNum = 20;
 		speed = 10;
 		canMove = true;
 
@@ -26,36 +35,40 @@ public class littleGuy: MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Prevent player from moving before the units reach the end of their travel
-		if (canMove) {
-			if (Input.GetKeyDown (KeyCode.A)) {
-				MoveLeft ();
-			} else if (Input.GetKeyDown (KeyCode.D)) {
-				MoveRight ();
-			} else if (Input.GetKeyDown (KeyCode.S)) {
-				MoveDown ();
-			} else if (Input.GetKeyDown (KeyCode.W)) {
-				MoveUp ();
-			}
-		} else {
-			if (fraction < 1) {
-				fraction = ((Time.time - startTime) * speed) / journeyLength;
-				transform.position = Vector2.Lerp (currPos, newPos, fraction);
-			}
+		//only move if the player has control of the unit 
+		if (hasControl) {
+			//Prevent player from moving before the units reach the end of their travel
+			if (canMove) {
+				if (Input.GetKeyDown (KeyCode.A)) {
+					MoveLeft ();
+				} else if (Input.GetKeyDown (KeyCode.D)) {
+					MoveRight ();
+				} else if (Input.GetKeyDown (KeyCode.S)) {
+					MoveDown ();
+				} else if (Input.GetKeyDown (KeyCode.W)) {
+					MoveUp ();
+				}
+			} else {
+				if (fraction < 1) {
+					fraction = ((Time.time - startTime) * speed) / journeyLength;
+					transform.position = Vector2.Lerp (currPos, newPos, fraction);
+				}
 
 
-		    myPos = transform.position;
-			if (myPos == newPos) {
-				canMove = true;
+				myPos = transform.position;
+				if (myPos == newPos) {
+					canMove = true;
+				}
 			}
 		}
-
+			
 	}
 
 	void MoveLeft()
 	{
 		currPos = transform.position;
 		newPos = new Vector2 (transform.position.x - moveNum, transform.position.y);
+		midPos = new Vector2 (transform.position.x - moveNum / 2, transform.position.y);
 		journeyLength = Vector2.Distance (currPos, newPos);
 		startTime = Time.time;
 		fraction = 0;
@@ -66,6 +79,7 @@ public class littleGuy: MonoBehaviour {
 	{
 		currPos = transform.position;
 		newPos = new Vector2 (transform.position.x + moveNum, transform.position.y);
+		midPos = new Vector2 (transform.position.x + moveNum/2, transform.position.y);
 		journeyLength = Vector2.Distance (currPos, newPos);
 		startTime = Time.time;
 		fraction = 0;
@@ -76,6 +90,7 @@ public class littleGuy: MonoBehaviour {
 	{
 		currPos = transform.position;
 		newPos = new Vector2 (transform.position.x, transform.position.y + moveNum);
+		midPos = new Vector2 (transform.position.x , transform.position.y + moveNum/2);
 		journeyLength = Vector2.Distance (currPos, newPos);
 		startTime = Time.time;
 		fraction = 0;
@@ -86,6 +101,7 @@ public class littleGuy: MonoBehaviour {
 	{
 		currPos = transform.position;
 		newPos = new Vector2 (transform.position.x, transform.position.y - moveNum);
+		midPos = new Vector2 (transform.position.x , transform.position.y - moveNum/2);
 		journeyLength = Vector2.Distance (currPos, newPos);
 		startTime = Time.time;
 		fraction = 0;
@@ -93,5 +109,30 @@ public class littleGuy: MonoBehaviour {
 	}
 
 
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "wall" || other.gameObject.tag == "alien") {
+			myPos = transform.position;
+			if (compareVector(myPos, currPos)) {
+				newPos = currPos;
+			} else if (compareVector(myPos, midPos)) {
+				newPos = midPos;
+			}
+			currPos = transform.position;
+		}
 
+	}
+
+	bool compareVector(Vector2 v1, Vector2 v2)
+	{
+		float xDif = Mathf.Abs(v1.x - v2.x);
+		float yDif = Mathf.Abs (v1.y - v2.y);
+
+
+		if (xDif < 1 && yDif < 1) {
+			return true;
+		} 
+		return false;
+
+	}
 }
