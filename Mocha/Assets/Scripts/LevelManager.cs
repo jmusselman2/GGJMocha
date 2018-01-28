56f0de4aef6a6d7477a16add09c2fb80a0b21e25
jmusselman <jmusselman21@gmail.com>
2018-01-28 00:28:51 -0700

    Start Menu

---
+++ Mocha/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public bool autoLoadNextLevel = false;
+	public float loadNextLevelAfter;
+
+	void Start()
+	{
+		if (autoLoadNextLevel) {
+			Invoke("LoadNextLevel", loadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest()
+	{
+		Application.Quit();
+	}
+
+	// Loads next level by index
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
